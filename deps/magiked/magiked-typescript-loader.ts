4a8457da51b7c851bb98511ce6a9d9042103bf49
Eric Miffre <eric.miffre@gmail.com>
2023-04-06 10:43:49 +0200

    Added the link/unlink devtool

--- deps/magiked/magiked-typescript-loader.ts
+++ deps/magiked/magiked-typescript-loader.ts
@@ -1,1 +1,1 @@
-export * from "https://deno.land/x/magiked_typescript_loader@v0.1.0/mod.ts"
+export * from /*devlink/ "../../../magiked-typescript-loader/mod.ts" /*/ "https://deno.land/x/magiked_typescript_loader@v0.1.0/mod.ts" /**/
